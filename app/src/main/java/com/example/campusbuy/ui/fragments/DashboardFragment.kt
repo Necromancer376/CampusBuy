1e280eef1d6536da0c9b3a082bcf724b76ebb7ea
Malhar Dharmadhikari <66357015+Necromancer376@users.noreply.github.com>
2022-06-14 22:26:15 +0530

    cleaning up code

--- app/src/main/java/com/example/campusbuy/ui/fragments/DashboardFragment.kt
+++ app/src/main/java/com/example/campusbuy/ui/fragments/DashboardFragment.kt
@@ -23,7 +23,6 @@ class DashboardFragment : BaseFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        requireActivity().window.statusBarColor = Color.BLACK
         setHasOptionsMenu(true)
     }
 
