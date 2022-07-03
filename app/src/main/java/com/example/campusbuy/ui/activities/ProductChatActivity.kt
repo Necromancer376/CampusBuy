5e542ddd5506dd6ab14622bb74930a57db40a88c
Malhar Dharmadhikari <66357015+Necromancer376@users.noreply.github.com>
2022-07-03 17:45:23 +0530

    starting chat

---
+++ app/src/main/java/com/example/campusbuy/ui/activities/ProductChatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.campusbuy.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.campusbuy.R
+
+class ProductChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_chat)
+    }
+}
