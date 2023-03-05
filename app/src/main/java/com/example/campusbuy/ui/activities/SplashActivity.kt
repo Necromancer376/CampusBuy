e25e3423d50cfb72c203d9ffbaa7472e8cb0c814
Malhar Dharmadhikari <66357015+Necromancer376@users.noreply.github.com>
2023-03-05 16:03:37 +0530

    minor change

--- app/src/main/java/com/example/campusbuy/ui/activities/SplashActivity.kt
+++ app/src/main/java/com/example/campusbuy/ui/activities/SplashActivity.kt
@@ -11,7 +11,7 @@ import com.example.campusbuy.R
 import com.google.firebase.auth.FirebaseAuth
 
 
-class SplashActivity : AppCompatActivity() {
+class 1SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
